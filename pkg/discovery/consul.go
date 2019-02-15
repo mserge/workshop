2937c583dbb53dae653f2aeb88355ae3c689153b
akartasov <akartasov@pushwoosh.com>
2019-02-15 13:55:23 +0700

    add discovery

--- pkg/discovery/consul.go
+++ pkg/discovery/consul.go
@@ -1,9 +1,17 @@
 package discovery
 
 import (
+	"errors"
 	"fmt"
+
 	"github.com/hashicorp/consul/api"
 )
+
+type ServiceNode struct {
+	ID      string
+	Address string
+	Port    int
+}
 
 func NewConsulClient(hostname string) (*api.Client, error) {
 	consulConfig := &api.Config{}
@@ -15,6 +23,25 @@ func NewConsulClient(hostname string) (*api.Client, error) {
 	}
 
 	return consulClient, nil
+}
+
+func GetServiceNode(consulClient *api.Client, serviceName string) (*ServiceNode, error) {
+	serviceEntry, _, err := consulClient.Health().Service(serviceName, "", true, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(serviceEntry) == 0 {
+		return nil, errors.New(fmt.Sprintf("service %s is not discovered\n", serviceName))
+	}
+
+	sn := &ServiceNode{
+		ID:      serviceEntry[0].Service.ID,
+		Address: serviceEntry[0].Service.Address,
+		Port:    serviceEntry[0].Service.Port,
+	}
+
+	return sn, nil
 }
 
 func RegisterService(consulClient *api.Client, cfg *config.Config) error {
